f60b8cfb47145a80148990bf331f555f0d1fdc67
cristoferapr <cpr.refor@gmail.com>
2024-11-04 19:08:07 -0300

    Footer added

---
+++ api/register.py
@@ -0,0 +1,28 @@
+from flask import request
+from flask_restful import Resource
+
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+
+        name = data.get('name')
+        email = data.get('email')
+        address = data.get('address')
+        password = data.get('password')
+        phone_number = data.get('phoneNumber')
+
+        if not name or not email or not address or not password or not phone_number:
+            return {"message": "Todos los campos son obligatorios."}, 400
+
+        # Aquí podrías agregar lógica para procesar y guardar los datos del registro
+        # Por ahora solo devolveremos los datos recibidos
+        return {
+            "message": "Registro exitoso",
+            "data": {
+                "name": name,
+                "email": email,
+                "address": address,
+                "password": password,
+                "phone_number": phone_number
+            }
+        }, 201
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Register from './components/Register';
+import Footer from './components/Footer';
+import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/search-result-page' element={<About />}></Route>
+            <Route exact path='/register' element={<Register />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
